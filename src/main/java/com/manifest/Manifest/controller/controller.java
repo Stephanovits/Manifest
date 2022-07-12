f7132e6d7ef8c9386ed85f0213e1fd07d955fffd
Stephan Schranz <stephan.schranz@me.com>
2022-07-12 14:44:03 +0100

    CREATED: First helloWorld() for testing

--- src/main/java/com/manifest/Manifest/controller/controller.java
+++ src/main/java/com/manifest/Manifest/controller/controller.java
@@ -1,6 +1,14 @@
 package com.manifest.Manifest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class controller {
 
+    @GetMapping(value = "/")
+    public String helloWorld() {
+        return "Welcome to Manifest";
+    }
 
 }
