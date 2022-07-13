4352c315d7120f584bcaeae351e1de4b540f022c
Stephan Schranz <stephan.schranz@me.com>
2022-07-13 09:58:05 +0100

    EDIT: Renamed controller to Controller

--- src/main/java/com/manifest/Manifest/controller/Controller.java
+++ src/main/java/com/manifest/Manifest/controller/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class controller {
+public class Controller {
 
     @Autowired
     private PatientTransportService patientTransportService;
