88eae3e527a2e71debe500af8a0aaa38053d4d91
Stephan Schranz <stephan.schranz@me.com>
2022-07-19 09:17:06 +0100

    ADDED: getAllPatientTransportsTEST()

--- src/test/java/com/manifest/Manifest/controller/ControllerTest.java
+++ src/test/java/com/manifest/Manifest/controller/ControllerTest.java
@@ -24,6 +24,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -101,7 +104,37 @@ class ControllerTest {
     }
 
     @Test
-    void getAllPatientTransportsTEST() {
+    void getAllPatientTransportsTEST() throws Exception {
+        List<PatientTransport> expected = new ArrayList<PatientTransport>();
+
+        PatientTransport pt1 = new PatientTransport();
+        pt1.setJobId(Integer.toUnsignedLong(1));
+        pt1.setPatientName("Huber");
+        pt1.setPatientWard("W1");
+        pt1.setPatientRoom("123");
+        pt1.setExamination("CD");
+        pt1.setStatus("Waiting");
+        pt1.setType("Routine");
+
+        PatientTransport pt2 = new PatientTransport();
+        pt2.setJobId(Integer.toUnsignedLong(2));
+        pt2.setPatientName("Weber");
+        pt2.setPatientWard("W2");
+        pt2.setPatientRoom("999");
+        pt2.setExamination("MR");
+        pt2.setStatus("Waiting");
+        pt2.setType("Emergency");
+
+        expected.add(pt1);
+        expected.add(pt2);
+
+        Mockito.when(patientTransportService.getAllPatientTransports()).thenReturn(expected);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/getAllPatientTransports"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].patientName").value(pt1.getPatientName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].patientName").value(pt2.getPatientName()));
+
     }
 
     @Test
