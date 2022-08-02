322f92b60f5f19d7a5556c3b9ca19758cebc9174
Stephan Schranz <stephan.schranz@me.com>
2022-08-02 16:31:16 +0100

    ADDS: filterChain() to SecurityConfiguration

--- src/main/java/com/manifest/Manifest/configuration/SecurityConfiguration.java
+++ src/main/java/com/manifest/Manifest/configuration/SecurityConfiguration.java
@@ -3,9 +3,11 @@ package com.manifest.Manifest.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,15 +18,33 @@ public class SecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         List<UserDetails> userList = new ArrayList<>();
+
         UserDetails user = User.withDefaultPasswordEncoder()
                 .username("user")
                 .password("user")
                 .roles("USER")
                 .build();
 
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
         userList.add(user);
+        userList.add(user2);
+
         return new InMemoryUserDetailsManager(userList);
     }
 
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/").permitAll().and().formLogin();
+
+        return http.build();
+    }
 
 }
