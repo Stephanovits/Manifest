42bd8fa21897fa78d8c9efdbff994e2c666fc316
Stephan Schranz <stephan.schranz@me.com>
2022-08-19 09:10:11 +0200

    ADDS: getAllExaminationsTest() and deleteExaminationByIdTest()

--- src/test/java/com/manifest/Manifest/service/ExaminationServiceTest.java
+++ src/test/java/com/manifest/Manifest/service/ExaminationServiceTest.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -32,6 +36,43 @@ public class ExaminationServiceTest {
         assertEquals(expected, accepted);
     }
 
+    @Test
+    void getExaminationByIdTest(){
+        //given
+        Examination testExam = new Examination();
+        testExam.setExaminationName("CD");
+        //when
+        Mockito.when(examinationRepository.findById(1L)).thenReturn(Optional.of(testExam));
+        //then
+        Examination expected = testExam;
+        Examination accepted = examinationService.getExaminationById(1L);
+        assertEquals(expected, accepted);
+    }
 
+    @Test
+    void getAllExaminationsTest(){
+        //given
+        Examination testExam = new Examination();
+        testExam.setExaminationName("CD");
 
+        Examination testExam2 = new Examination();
+        testExam2.setExaminationName("MR");
+
+        List<Examination> expected = new ArrayList<>();
+        expected.add(testExam2);
+        expected.add(testExam);
+        //when
+        Mockito.when(examinationRepository.findAll()).thenReturn(expected);
+        //then
+        List<Examination> accepted = examinationService.getAllExaminations();
+        // - also testing for correct sorting
+        assertEquals(expected.get(0).getExaminationName(), "CD");
+    }
+
+    @Test
+    void deleteExaminationByIdTest(){
+        //when
+        examinationService.deleteExaminationById(1L);
+        Mockito.verify(examinationRepository).deleteById(1L);
+    }
 }
