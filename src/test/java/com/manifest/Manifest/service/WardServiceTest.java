07439bbcd8af1551348c0478e8dbfad887e4fa7e
Stephan Schranz <stephan.schranz@me.com>
2022-08-19 09:18:38 +0200

    ADDS: WardSeerviceTest

---
+++ src/test/java/com/manifest/Manifest/service/WardServiceTest.java
@@ -0,0 +1,39 @@
+package com.manifest.Manifest.service;
+
+import com.manifest.Manifest.model.Ward;
+import com.manifest.Manifest.repository.WardRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class WardServiceTest {
+
+    @Autowired
+    WardService wardService;
+
+    @MockBean
+    WardRepository wardRepository;
+
+    @Test
+    void saveWardTest(){
+        //GIVEN
+        Ward testWard = new Ward();
+        testWard.setWardName("W1");
+        //WHEN
+        Mockito.when(wardRepository.save(testWard)).thenReturn(testWard);
+        //THEN
+        Ward expected = testWard;
+        Ward actual = wardService.saveWard(testWard);
+        assertEquals(expected, actual);
+    }
+
+
+
+
+
+}
